0889e2cfc1ac6de697b768f74e94c734691d6864
Ivan Ivan <ivanov.ivan.kr@gmail.com>
2024-02-18 15:12:16 +0200

    For Loop

---
+++ 07.ForLoop/09.LeftAndRightSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] firstNumbers = new int[n];
+        int[] secondNumbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+
+            firstNumbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+
+            secondNumbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            leftSum += firstNumbers[i];
+            rightSum += secondNumbers[i];
+        }
+
+        int diff = Math.Abs(leftSum - rightSum);
+
+        if (leftSum == rightSum)
+        {
+            Console.WriteLine("Yes, sum = " + leftSum);
+        }
+        else
+        {
+            Console.WriteLine("No, diff = " + diff);
+        }
+    }
+}
---
+++ 07.ForLoop/10.OddEvenSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0) 
+                evenSum += numbers[i];
+            else 
+                oddSum += numbers[i];
+        }
+
+        int diff = Math.Abs(evenSum - oddSum);
+
+        if (evenSum == oddSum)
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine("Sum = " + evenSum);
+        }
+        else
+        {
+            Console.WriteLine("No");
+            Console.WriteLine("Diff = " + diff);
+        }
+    }
+}
